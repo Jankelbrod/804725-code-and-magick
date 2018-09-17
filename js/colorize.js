9eb0108377dabc1620c9534e93fd7fb1ccb03834
Anastasia Triboi <jankelbrod@gmail.com>
2018-09-18 00:46:43 +0500

    Учебный проект: в движении

--- js/colorize.js
+++ js/colorize.js
@@ -3,7 +3,7 @@
 (function () {
   window.colorize = function (element, colors, selector) {
     element.addEventListener('click', function () {
-      var color = window.dialog.getRandomElement(colors);
+      var color = window.util.getRandomElement(colors);
       if (element.tagName.toLowerCase() === 'div') {
         element.style.backgroundColor = color;
       } else {
