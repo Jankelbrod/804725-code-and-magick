85565919ac44146e941acc3279dfe9d4dc80b350
Anastasia Triboi <jankelbrod@gmail.com>
2018-09-17 23:22:28 +0500

    Учебный проект: разделяй и властвуй

--- js/colorize.js
+++ js/colorize.js
@@ -2,14 +2,14 @@
 
 (function () {
   window.colorize = function (element, colors, selector) {
-  element.addEventListener('click', function () {
-    var color = window.dialog.getRandomElement(colors);
-    if (element.tagName.toLowerCase() === 'div') {
-      element.style.backgroundColor = color;
-    } else {
-      element.style.fill = color;
-    }
+    element.addEventListener('click', function () {
+      var color = window.dialog.getRandomElement(colors);
+      if (element.tagName.toLowerCase() === 'div') {
+        element.style.backgroundColor = color;
+      } else {
+        element.style.fill = color;
+      }
       selector.value = color;
-  });
-};
+    });
+  };
 })();
